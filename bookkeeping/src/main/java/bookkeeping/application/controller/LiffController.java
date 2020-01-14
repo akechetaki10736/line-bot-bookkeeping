2c64eeb52e31a6ca1bdcb2f6e57fa84af893c139
Ethan <Ethan.Wu@ttpsc.com>
2020-01-14 18:57:09 +0800

    Personal Accounting liff app.

--- bookkeeping/src/main/java/bookkeeping/application/controller/LiffController.java
+++ bookkeeping/src/main/java/bookkeeping/application/controller/LiffController.java
@@ -1,6 +1,7 @@
 package bookkeeping.application.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class LiffController {
     @GetMapping("/demo")
     public String getIndexHTML() {
         return "index";
+    }
+
+    @GetMapping("/personalAccount")
+    public String getAccount(Model model) {
+            return "personalAccount";
     }
 }
---
+++ bookkeeping/src/main/resources/static/account.js
@@ -0,0 +1,45 @@
+$(function() {
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "1653663005-PaNGqQKe";   // change the default LIFF value if you are not using a node server
+
+    // DO NOT CHANGE THIS
+    let myLiffId = "";
+
+	myLiffId = defaultLiffId;
+	initializeLiffOrDie(myLiffId);
+/**
+* Check if myLiffId is null. If null do not initiate liff.
+* @param {string} myLiffId The LIFF ID of the selected element
+*/
+function initializeLiffOrDie(myLiffId) {
+        initializeLiff(myLiffId);
+}
+
+/**
+* Initialize LIFF
+* @param {string} myLiffId The LIFF ID of the selected element
+*/
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(function ()  {
+            $('#browserLanguage').html(liff.getLanguage());
+            $('#sdkVersion').html(liff.getVersion());
+            $('#isInClient').html(liff.isInClient());
+            $('#isLoggedIn').html(liff.isLoggedIn());
+            $('#deviceOS').html(liff.getOS());
+            liff.getProfile().then(function(profile) {
+                $('#UID').html(profile.userId);
+                $('#Name').html(profile.displayName);
+            }).catch(function(error) {
+                alert('Error getting profile: ' + error);
+            });
+        })
+        .catch((err) => {
+			alert('liff sdk init error! err : ' + err);
+        });
+}
+
+});
