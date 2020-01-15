7b97e63bd25a18ac2244d6269e885fae97782e78
kit <tingiao.ki@ttpsc.com>
2020-01-15 13:59:59 +0800

    ajax demo.

--- bookkeeping/src/main/java/bookkeeping/application/controller/LiffController.java
+++ bookkeeping/src/main/java/bookkeeping/application/controller/LiffController.java
@@ -1,12 +1,15 @@
 package bookkeeping.application.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@Slf4j
 public class LiffController {
 
     @GetMapping("/demo")
@@ -17,5 +20,12 @@ public class LiffController {
     @GetMapping("/personalAccount")
     public String getAccount(Model model) {
             return "personalAccount";
+    }
+
+    @GetMapping("/query")
+    @ResponseBody
+    public String test(@RequestParam String UID){
+        log.warn(UID);
+        return "foo";
     }
 }
--- bookkeeping/src/main/resources/static/account.js
+++ bookkeeping/src/main/resources/static/account.js
@@ -1,6 +1,6 @@
 $(function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1653663005-PaNGqQKe";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1653663188-x3zv4LO8";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
@@ -32,6 +32,11 @@ function initializeLiff(myLiffId) {
             $('#deviceOS').html(liff.getOS());
             liff.getProfile().then(function(profile) {
                 $('#UID').html(profile.userId);
+                $.get("https://3f49adcb.ngrok.io/query",
+                    {UID: profile.userId},
+                    function (data) {
+                        alert("Data Loaded: " + data);
+                    });
                 $('#Name').html(profile.displayName);
             }).catch(function(error) {
                 alert('Error getting profile: ' + error);
